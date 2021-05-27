5732140647622537a4d2a02170d1b072a7b814c8
Craig5117 <craig.bennett5117@gmail.com>
2021-05-27 15:51:53 -0400

    added swap force accessories and discs

--- prisma/accessories/swapAccessories.js
+++ prisma/accessories/swapAccessories.js
@@ -1,5 +1,103 @@
 const swapAccessories = [
-
+    {
+        accName: "Tower of Time",
+        gameId: 3,
+        setId: 7,
+        value: 6.00,
+    },
+    {
+        accName: "Battle Hammer",
+        gameId: 3,
+        setId: 7,
+        value: 1.50,
+    },
+    {
+        accName: "Sky Diamond",
+        gameId: 3,
+        setId: 7,
+        value: 2.00,
+    },
+    {
+        accName: "Sheepwreck Island",
+        gameId: 3,
+        setId: 8,
+        value: 10.00,
+    },
+    {
+        accName: "Platinum Sheep",
+        gameId: 3,
+        setId: 8,
+        value: 1.00,
+    },
+    {
+        accName: "Groove Machine",
+        gameId: 3,
+        setId: 8,
+        value: 4.00,
+    },
+    {
+        accName: "Fiery Forge",
+        gameId: 3,
+        setId: 9,
+        value: 1.00,
+    },
+    {
+        accName: "Arkeyan Crossbow",
+        gameId: 3,
+        setId: 10,
+        value: 0.50,
+    },
+    // swap discs and portals
+    {
+        accName: "Swap Force 3DS Cart",
+        gameId: 3,
+        value: 6.00,
+    }, 
+    {
+        accName: "Swap Force 3DS Portal",
+        gameId: 3,
+        modelNo: "84721790",
+        value: 10.00,
+    },
+    {
+        accName: "Swap Force PS3 Disc",
+        gameId: 3,
+        value: 3.00,
+    }, 
+    {
+        accName: "Swap Force PS3/Wii Portal",
+        gameId: 3,
+        modelNo: "0000547",
+        value: 10.00,
+    },
+    {
+        accName: "Swap Force PS4 Disc",
+        gameId: 3,
+        value: 10.00,
+    }, 
+    {
+        accName: "Swap Force Wii Disc",
+        gameId: 3,
+        value: 4.00,
+    }, 
+    {
+        accName: "Swap Force XBOX 360 Disc",
+        gameId: 3,
+        value: 6.00,
+    }, 
+    {
+        accName: "Swap Force XBOX One Disc",
+        gameId: 3,
+        value: 15.00,
+    },
+    {
+        accName: "Swap Force XBOX 360 Portal",
+        gameId: 3,
+        modelNo: "84704790",
+        value: 6.00,
+    },
+     
+    
 ];
 
 module.exports = swapAccessories;
--- prisma/set.js
+++ prisma/set.js
@@ -25,6 +25,23 @@ const sets = [
         setName: "Giants Battlepack 2",
         gameId: 2
     },
+    // swap sets
+    {
+        setName: "Swap Adventure Pack 1",
+        gameId: 3
+    },
+    {
+        setName: "Swap Adventure Pack 2",
+        gameId: 3
+    },
+    {
+        setName: "Swap Battlepack 1",
+        gameId: 3
+    },
+    {
+        setName: "Swap Battlepack 2",
+        gameId: 3
+    },
 ];
 
 module.exports = sets;
